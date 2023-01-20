aa40293ec093fe82b6ea69399a470a42f7f2caac
Avinash Mairal <iauro@Avinashs-MacBook-Air.local>
2023-01-20 17:04:13 +0530

    updated keyclock variables

--- lib/src/file_content/resourse/keyclock_vars.dart
+++ lib/src/file_content/resourse/keyclock_vars.dart
@@ -1,19 +1,15 @@
 class KeyclockVarsContent {
   static const String keyclockVarsContent = '''class KeyClockConstants {
-  static const database = "63b1c88f4466e083992b2a59";
+  static const database = "testDB";
   static const clientId = "masterClient";
-
-  // static const DATABASE = "TestRealm";
-  // static const CLIENT_ID = "testClient";
 
 }
 
 class KeyClockUrl {
   //need to change as per project
-  // static const authServerUrl = "http://0.0.0.0:8080/";
-  // static const redirectUrl = "gessa.io:/oauth2redirect";
-  static const authServerUrl = "https://auth-dev.gessa.io/";
-  static const redirectUrl = "test.io:/oauth2redirect";
+  static const authServerUrl = "http://0.0.0.0:8080/";
+  static const redirectUrl = "gessa.io:/oauth2redirect";
+
   //remains same for every project
   static var login = "/protocol/openid-connect/auth";
   static var logout = "/protocol/openid-connect/logout";
